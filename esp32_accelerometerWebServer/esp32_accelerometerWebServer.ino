dbd586d6a46b1f804877b303ce144d5c0ac576c0
khairul akmal <cayrul272@gmail.com>
2024-08-22 16:01:06 +0800

    proximity 3 & esp32 accelerometer web server - data boleh diambil dari IP - Working

---
+++ esp32_accelerometerWebServer/esp32_accelerometerWebServer.ino
@@ -0,0 +1,91 @@
+#include <WiFi.h>
+#include <ESPAsyncWebServer.h>
+#include <SoftwareSerial.h>
+#include <DFRobot_WT61PC.h>
+
+
+#define rxPin 16
+#define txPin 17
+#define FPSerial mySerial
+
+// Replace with your network credentials
+const char* ssid = "Cayrol";
+const char* password = "blok-f1108";
+
+IPAddress staticIP(192, 168, 169, 146);
+IPAddress gateway(192, 168, 169, 27);
+IPAddress subnet(255, 255, 255, 0);
+// IPAddress dns(192, 168, 1, 254);
+
+// Create an AsyncWebServer object on port 80
+AsyncWebServer server(80);
+
+// Simulate a sensor value
+int sensorValue = 123;
+
+// Set up a new SoftwareSerial object
+// SoftwareSerial mySerial =  SoftwareSerial(rxPin, txPin);
+SoftwareSerial mySerial(rxPin, txPin);
+
+DFRobot_WT61PC sensor(&FPSerial);
+
+
+void wifiSetup(){
+    // Connect to Wi-Fi network with SSID and password
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  // Print local IP address and start web server
+  Serial.println("");
+  Serial.println("WiFi connected.");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+  server.begin();
+}
+
+void setup(){
+  Serial.begin(9600);
+  // Define pin modes for TX and RX
+  pinMode(rxPin, INPUT);
+  pinMode(txPin, OUTPUT);
+    // Set the baud rate for the SoftwareSerial object
+  mySerial.begin(9600);
+  sensor.modifyFrequency(FREQUENCY_200HZ);
+
+  wifiSetup();
+
+  // Route for root / web page
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+    // String response = "Sensor Value: " + String(sensorValue);
+    String response = "api_key=tPmAT5Ab3j7F9&sensor=DFROBOT&acc_x="+ String(sensor.Acc.X) +"&acc_y=" + String(sensor.Acc.Y) +"&acc_z="+ String(sensor.Acc.Z);
+    request->send(200, "text/plain", response);
+  });
+
+  // Start server
+  server.begin();
+}
+
+void loop(){
+  // Simulate updating sensor value
+ // Example: read from analog pin 34
+  readSensor();
+  // sensorValue = sensor.Acc.X;
+  delay(1000);
+}
+
+void readSensor(){
+
+    if (sensor.available()) {
+    Serial.print("Acc\t"); Serial.print(sensor.Acc.X); Serial.print("\t");
+    Serial.print(sensor.Acc.Y); Serial.print("\t"); Serial.println(sensor.Acc.Z); //acceleration information of X,Y,Z
+    Serial.print("Gyro\t"); Serial.print(sensor.Gyro.X); Serial.print("\t");
+    Serial.print(sensor.Gyro.Y); Serial.print("\t"); Serial.println(sensor.Gyro.Z); //angular velocity information of X,Y,Z
+    Serial.print("Angle\t"); Serial.print(sensor.Angle.X); Serial.print("\t");
+    Serial.print(sensor.Angle.Y); Serial.print("\t"); Serial.println(sensor.Angle.Z); //angle information of X, Y, Z 
+    Serial.println();
+  }
+}
--- proximity3/proximity3.ino
+++ proximity3/proximity3.ino
@@ -1,5 +1,20 @@
+#include <WiFi.h>
+#include <ESPAsyncWebServer.h>
+
 // Define the GPIO pin connected to the pulse sensor
 #define PULSE_SENSOR_PIN 26  // Change this to your actual GPIO pin
+
+// Replace with your network credentials
+const char* ssid = "Cayrol";
+const char* password = "blok-f1108";
+
+IPAddress staticIP(192, 168, 169, 147);
+IPAddress gateway(192, 168, 169, 27);
+IPAddress subnet(255, 255, 255, 0);
+// IPAddress dns(192, 168, 1, 254);
+
+// Create an AsyncWebServer object on port 80
+AsyncWebServer server(80);
 
 // Variables for pulse counting and debounce
 volatile unsigned long lastPulseTime = 0;
@@ -7,6 +22,24 @@ volatile unsigned int pulseCount = 0;
 const unsigned long debounceDelay = 5;  // Debounce time in milliseconds
 int currentButtonState = 0;
 int buttonState = 0;
+int finalCount = 0;
+
+void wifiSetup(){
+    // Connect to Wi-Fi network with SSID and password
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  // Print local IP address and start web server
+  Serial.println("");
+  Serial.println("WiFi connected.");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+  server.begin();
+}
 
 void IRAM_ATTR pulseISR() {
   unsigned long currentTime = millis();
@@ -43,6 +76,19 @@ void setup() {
 
   // Attach the interrupt to the pulse sensor pin
   attachInterrupt(digitalPinToInterrupt(PULSE_SENSOR_PIN), pulse, FALLING);
+
+
+  wifiSetup();
+
+  // Route for root / web page
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+    // String response = "Sensor Value: " + String(sensorValue);
+    String response = "api_key=mkaa2009&sensor=COUNTESP32-147&count="+ String(finalCount);
+    request->send(200, "text/plain", response);
+  });
+
+  // Start server
+  server.begin();
 }
 
 void loop() {
@@ -53,6 +99,7 @@ void loop() {
   if (currentTime - lastPrintTime >= 10000) {
     Serial.print("Pulse Count: ");
     Serial.println(pulseCount);
+    finalCount = pulseCount;
 
     // Reset the count after printing if needed
     pulseCount = 0;
